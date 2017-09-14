1b27cbf1864e648b7bf3151b44c2d69b1124dfe1
Jack Wood <gritteh@gmail.com>
2017-09-14 22:00:50 +0100

    Added a lot of comments in javascript for readability

--- js/script.js
+++ js/script.js
@@ -9,92 +9,142 @@ $(document).ready(function() {
     let buttonArray = [];
     let deleteSelectorArray = [];
 
-    // Format all added players into 4 x * grid
-    const formatAll = array => {
-        return array.map((box, i) => {
+    // Format an array of elements into a formatted array of elements
+    const formatAll = array => array.map((box, i) => {
             box.css({
                 "left": (i % 4) * 25 + 4 + "%",
                 "top": Math.floor(i/4) * 50 + "px"
             });
-        })
-    };
+        });
 
-    // function to assign classes so I can remove
+    // Give each button element in an array good class names for targetting
     const giveAllButtonsClass = buttonArray => buttonArray.map((button, i) => button.addClass("del_button_" + i));
 
-
-    // function to create new jQuery objects of delete buttons
+    // Create jQuery selectors for use with buttons
     const createDeleteSelector = className => $(className);
-///
 
+    // When delete button clicked, delete the relevant button in all arrays
+    let onClickFunction = (index) => {
+        buttonArray.splice(index, 1);
+        playersHtmlArray.splice(index, 1);
+        playersArray.splice(index, 1);
+        deleteSelectorArray.splice(index, 1);
 
-    // function for on button click
-    let onClickFunction = (i) => {
-        buttonArray.splice(i, 1);
-        playersHtmlArray.splice(i, 1);
-        playersArray.splice(i, 1);
-        deleteSelectorArray.splice(i, 1);
         renderGrid();
-        console.log('clicked');
     };
-    // adding players
+
+    // Adding players
     playerForm.submit(function(e) {
+        // Prevent page from refreshing
         e.preventDefault();
+
         if (!nameInput.val() == "") {
+            // Get the name
             let newName = nameInput.val();
+
+            // Add name to an array
             playersArray.push(newName);
+
+            // Reset the Input text
             nameInput.val("");
-            // console.log(playersArray);
-            // playersHtmlArray = mapArrayToGraph(playersArray);
+
+            // buttonArray's length acting as index of new player
             let buttonArrayLength = buttonArray.length;
+
+            // HTML elements for player
             let container = $("<div/>").addClass("new-player");
             let name = $("<p/>").addClass("new-player__name");
-            let button = $("<button/>").addClass("new-player__remove fa fa-times").text("").on("click", () => onClickFunction(buttonArrayLength));
+
+            // Creates delete button, and adds on click event
+            let button = $("<button/>")
+                .addClass("new-player__remove fa fa-times")
+                .text("")
+                .on("click", () => onClickFunction(buttonArrayLength));
+
+            // Adds button to an array
             buttonArray.push(button);
+
+            // Make the <p> element's text the new player's name
             name.text(newName);
+
+            // A new array of buttons but with sequential class names
             let finalButtonArray = giveAllButtonsClass(buttonArray);
-            //[button, button, button]
+
+            // Find the current button in that array
             let currentButton = finalButtonArray[buttonArrayLength];
-            // creating array of jQuery delete selectors
+
+            // Creating array of jQuery delete button selectors using their class names
             deleteSelectorArray.push(createDeleteSelector(currentButton.attr("class")));
-///
-            //
+
+            // Append the elements in the right structure
             container.append(name).append(currentButton);
 
-            // let formattedContainer = formatNewBox(container);
+            // Push the full HTML into an array
             playersHtmlArray.push(container);
-            formatAll(playersHtmlArray);
-            let newestName = playersHtmlArray[playersHtmlArray.length - 1];
-            playersTable.append(newestName);
-            console.log(playersHtmlArray);
 
+            // Format all elements into a grid
+            formatAll(playersHtmlArray);
+
+            // Find the newest element to add
+            let newestName = playersHtmlArray[playersHtmlArray.length - 1];
+
+            // Append onto players table
+            playersTable.append(newestName);
         }
     });
 
 
-    // render grid function for after deleting
+    // Re-render the grid, for after an element is deleted
     const renderGrid = () => {
+
+        // Remove all HTML elements inside the container
         playersTable.children().remove();
+
+        // Reset arrays to start fresh
         buttonArray = [];
         deleteSelectorArray = [];
         playersHtmlArray = [];
+
+        // Map the players array into elements
         playersArray.map((player, i) => {
+
+            // Array length acting as index
             let buttonArrayLength = buttonArray.length;
+
             let container = $("<div/>").addClass("new-player");
             let name = $("<p/>").addClass("new-player__name");
-            let button = $("<button/>").addClass("new-player__remove fa fa-times").text("").on("click", () => onClickFunction(buttonArrayLength));
+            let button = $("<button/>")
+                .addClass("new-player__remove fa fa-times")
+                .text("")
+                .on("click", () => onClickFunction(buttonArrayLength));
+
+            // Pushing button onto array
             buttonArray.push(button);
+
+            // Setting name as element text
             name.text(playersArray[i]);
+
+            // Giving buttons targetable class names
             let finalButtonArray = giveAllButtonsClass(buttonArray);
-            //[button, button, button]
+
             let currentButton = finalButtonArray[buttonArrayLength];
+
             // creating array of jQuery delete selectors
             deleteSelectorArray.push(createDeleteSelector(currentButton.attr("class")));
-            //
+
+            // Make HTML structure
             container.append(name).append(currentButton);
+
+            // Push it onto an array
             playersHtmlArray.push(container);
+
+            // Format the array
             formatAll(playersHtmlArray);
+
+            // Find newest
             let newestName = playersHtmlArray[playersHtmlArray.length - 1];
+
+            // Add it to the HTML
             playersTable.append(newestName);
         });
     }
